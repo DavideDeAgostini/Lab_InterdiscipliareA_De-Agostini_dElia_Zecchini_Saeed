fea6356f483c678d8ad7548e6824e556e92f1d7e
Davide De Agostini <65593904+DavideDeAgostini@users.noreply.github.com>
2026-05-21 14:04:47 +0200

    Update Prenotazione.java

---
+++ src/cinemax/model/Prenotazione.java
@@ -0,0 +1,11 @@
+    // metodo per prendere le info dal file csv
+
+    // arrylist dove salvare la ricerca
+
+    // metodo per cercare tramite nome/genere
+
+    // metodo per cercare tramite data e ora
+
+    // metodo per cercare solo tramite data
+
+    // metodo per prendere l'ora
